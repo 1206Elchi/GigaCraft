0d9ef56de1434e8d2886ab21943088b60c582c32
1206Elchi <elchi.der.zweite@gmail.com>
2025-04-04 17:41:59 +0200

    Added 2 Items from the Kaupenjoe Modding Tutorial (Textures, languages) and registered 1 of them.

--- src/main/java/net/elchi/gigacraftmod/GigaCraftMod.java
+++ src/main/java/net/elchi/gigacraftmod/GigaCraftMod.java
@@ -1,5 +1,6 @@
 package net.elchi.gigacraftmod;
 
+import net.elchi.gigacraftmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class GigaCraftMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
