c491fb4c9faba8f77c025577d6f16ff83bfc55a0
1206Elchi <elchi.der.zweite@gmail.com>
2025-04-04 17:15:41 +0200

    Initial commit

---
+++ src/main/java/net/elchi/gigacraftmod/GigaCraftMod.java
@@ -0,0 +1,16 @@
+package net.elchi.gigacraftmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GigaCraftMod implements ModInitializer {
+	public static final String MOD_ID = "gigacraftmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/elchi/gigacraftmod/GigacraftModClient.java
@@ -0,0 +1,10 @@
+package net.elchi.gigacraftmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GigacraftModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
